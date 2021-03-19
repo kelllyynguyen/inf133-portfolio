7715878777ffdf481cd7b0aee9cdab160ee5b270
kelllyynguyen <56004578+kelllyynguyen@users.noreply.github.com>
2021-03-18 18:56:00 -0700

    march 2021 update

---
+++ script2.js
@@ -0,0 +1,15 @@
+  //Get the button
+  var mybutton = document.getElementById("myBtn");
+
+  console.log(mybutton)
+  
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
